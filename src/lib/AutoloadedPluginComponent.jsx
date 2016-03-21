296ebf9642d844b8f60e4666732e11aaf67dfbe1
Andy Edwards <jedwards@fastmail.com>
2016-03-21 16:03:11 -0500

    initial commit

---
+++ src/lib/AutoloadedPluginComponent.jsx
@@ -0,0 +1,114 @@
+/* @flow */
+
+import React, {Component, PropTypes} from 'react';
+import Immutable from 'immutable'
+import {connect} from 'react-redux';
+import warning from 'fbjs/lib/warning';
+
+import {pluginActions, pluginTypes} from 'redux-plugins-immutable';
+const {LOADED, LOADING, NOT_LOADED} = pluginTypes;
+const {loadPlugin} = pluginActions;
+
+type SkinProps = {
+  loading?: boolean,
+  pluginName?: string,
+  loadError?: Error | string,
+  children?: any
+};
+
+class DefaultSkin extends Component<void,SkinProps,void> {
+  render(): ReactElement {
+    let {loading, pluginName, loadError, children} = this.props;
+
+    if (loading) {
+      return <h1>Loading {pluginName} plugin...</h1>;
+    }
+    if (loadError) {
+      return <h1>Failed to load {pluginName} plugin: {loadError.message || loadError}</h1>;
+    }
+    if (!children) {
+      return <h1>{`Couldn't find component for ${pluginName} plugin`}</h1>;
+    }
+    return children;
+  } 
+}
+
+type Props = {
+  pluginKey: string,
+  plugin?: Immutable.Map,
+  componentKey?: string,
+  getComponent?: (plugin: Immutable.Map) => Component<any,any,any>,
+  componentProps?: Object,
+  dispatch: Function,
+};
+
+/**
+ * Renders a Component from a plugin that may not be loaded.  If it is not loaded,
+ * this will show a loading alert and dispatch an action to load the plugin, rendering
+ * the Component when the plugin finishes loading.
+ *
+ * All you have to do is provide the pluginKey prop, and one of the following two props
+ * that specify what Component from the plugin to use:
+ * * componentKey prop - use plugin.getIn(['components', componentKey])
+ * * getComponent prop - use getComponent(plugin)
+ */
+class AutoloadedPluginComponent extends Component<void,Props,void> {
+  static contextTypes = {
+    AutoloadedPluginComponentSkin: PropTypes.any
+  };
+  getComponent: (plugin: Immutable.Map) => any = plugin => {
+    let {getComponent, componentKey} = this.props;
+    if (getComponent) {
+      return getComponent(plugin);
+    }
+    if (componentKey) {
+      return plugin.getIn(['components', componentKey]);
+    }
+  };
+  componentWillReceiveProps(nextProps: Props) {
+    let {getComponent, componentKey} = this.props;
+    warning(getComponent || componentKey, 'you must provide a getComponent or componentKey');
+  }
+  componentWillMount() {
+    let {plugin, pluginKey, dispatch} = this.props;
+    if (plugin && plugin.get('loadStatus') === NOT_LOADED && !this.getComponent(plugin)) {
+      dispatch(loadPlugin(pluginKey));
+    }
+  }
+  render() {
+    let {pluginKey, plugin, componentKey, componentProps} = this.props;
+    let skin = this.context.AutoloadedPluginComponentSkin || DefaultSkin;
+
+    let pluginName = pluginKey;
+    let component, loading, loadError;
+
+    if (plugin) {
+      pluginName = plugin.get('name');
+      component = this.getComponent(plugin);
+      loading   = plugin.get('loadStatus') === LOADING;
+      loadError = plugin.get('loadError');
+
+      if (React.isValidElement(component) && component) {
+        if (componentProps) {
+          component = React.cloneElement(component, componentProps);
+        }
+      }
+      else if (component) {
+        component = React.createElement(component, componentProps || {});
+      }
+    }
+    else {
+      loadError = `plugin ${pluginKey} not found`;
+    }
+
+    return React.createElement(skin, {pluginName, loading, loadError, children: component});
+  }
+}
+
+function select(state, props) {
+  return {
+    plugin: state.getIn(['plugins', props.pluginKey])
+  };
+}
+
+export default connect(select)(AutoloadedPluginComponent);
---
+++ src/lib/PluginComponents.jsx
@@ -0,0 +1,93 @@
+/* @flow */
+
+import React, {Component} from 'react';
+import Immutable from 'immutable'
+import {connect} from 'react-redux';
+import {createSelector} from 'reselect';
+
+import warning from 'fbjs/lib/warning';
+
+import createOrCloneElement from './util/createOrCloneElement';
+
+type Props = {
+  plugins?: Immutable.Map,
+  sortPlugins?: (plugins: Immutable.Map) => Immutable.Map,
+  componentKey?: string,
+  getComponent?: (plugin: Immutable.Map) => Component<any,any,any>,
+  componentProps?: Object,
+  children?: any
+};
+
+/**
+ * A shell into which plugins can inject components.  For example if you had:
+ *
+ * import PluginComponents from './PluginComponents';
+ *
+ * const Menu = ({children}) => <ul>
+ *   <li><a>Input Settings</a></li>
+ *   <li><a>Output Settings</a></li>
+ *   <li divider/>
+ *   {children}
+ * </ul>;
+ *
+ * <Dropdown component="div">
+ *   <Button caret>Settings</Button>
+ *   <PluginComponents componentKey="SettingsMenuItems">{Menu}</PluginComponents>
+ * </Dropdown>
+ *
+ * Then anything at the path ['components', 'SettingsMenuItems'] in any plugin will
+ * be passed in the children to the created <Menu> element.  This way it's easy for
+ * plugins to extend the UI in arbitrary places.
+ *
+ * There are two ways of specifying what component to get from each plugin:
+ * * componentKey prop - use plugin.getIn(['components', componentKey])
+ * * getComponent prop - use getComponent(plugin)
+ *
+ * You can specify a sortPlugins prop to control the order in which plugin components
+ * show.
+ */
+class PluginComponents extends Component<void,Props,void> {
+  componentWillReceiveProps(nextProps: Props) {
+    let {getComponent, componentKey} = nextProps;
+    warning(getComponent || componentKey, "you must provide either getComponent or componentKey");
+  }
+  selectPluginComponents: (props: Props) => ?Array<?ReactElement> = createSelector(
+    props => props.plugins,
+    props => props.sortPlugins,
+    ({getComponent, componentKey}) => {
+      if (getComponent)   return getComponent;
+      if (!componentKey)  return () => undefined;
+      return plugin => plugin.getIn(['components', componentKey]);
+    },
+    props => props.componentProps || {},
+    (plugins, sortPlugins, getComponent, componentProps) => {
+      if (plugins) {
+        if (sortPlugins) plugins = sortPlugins(plugins);
+
+        return plugins.map((plugin, key) => {
+          let component = plugin && getComponent(plugin);
+          return component && createOrCloneElement(component, {...componentProps, key});
+        }).toArray();
+      }
+    }
+  );
+  render() {
+    let {children} = this.props;
+    let pluginComponents = this.selectPluginComponents(this.props);
+
+    if (children) {
+      return React.createElement(children, {...this.props, children: pluginComponents});
+    }
+    return <div {...this.props}>
+      {pluginComponents}
+    </div>;
+  }
+}
+
+function select(state) {
+  return {
+    plugins: state.get('plugins')
+  };
+}
+
+export default connect(select)(PluginComponents);
